ab3192b183c8f62df1db1bd8699ce27e5320b775
Lilly Guo <reubenh14@tutanota.com>
2024-07-03 18:55:50 +0200

    login with civic canister derivationOrigin

--- src/civic_sign_frontend_demo/src/config.ts
+++ src/civic_sign_frontend_demo/src/config.ts
@@ -5,6 +5,8 @@ console.log('isProduction:', isProduction);
 const internetIdentityCanisterId = import.meta.env.VITE_INTERNET_IDENTITY_CANISTER_ID;
 const host = import.meta.env.VITE_HOST;
 
+const civicCanisterBackendUrl = "https://icp.civic.com/";
+
 console.log('env', { internetIdentityCanisterId, host }, import.meta.env.VITE_ENV);
 
 const internetIdentityUrl = isProduction
@@ -13,5 +15,6 @@ const internetIdentityUrl = isProduction
 
 export const config = {
   internetIdentityUrl,
+  civicCanisterBackendUrl,
   internetIdentityCanisterId,
 };
