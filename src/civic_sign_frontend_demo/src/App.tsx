5b3a07a81d8f82fa1de43a52f8f8d28cf27f3ff8
TYRONE AVNIT <tyronemichael@gmail.com>
2024-08-26 14:45:31 -0500

    TECH-292 - Update the RP to point to production

--- src/civic_sign_frontend_demo/src/App.tsx
+++ src/civic_sign_frontend_demo/src/App.tsx
@@ -60,7 +60,6 @@ function App() {
               principal={principal}
               gatekeeperNetwork={selectedGatekeeperNetwork.address}
               onCredentialCheck={handleCredentialCheck}
-              config={{ stage: 'dev' }}
             />
           </div>
         ) : (
